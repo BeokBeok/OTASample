467d6167570833727083263b930a1dd0e79645a4
BeokBeok <kekemusa37@gmail.com>
2022-10-15 23:40:11 +0900

    [적용] 앱 실행시 lokalise 업데이트

--- app/src/main/java/com/beok/otasample/MainActivity.kt
+++ app/src/main/java/com/beok/otasample/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.beok.otasample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lokalise.sdk.Lokalise
+import com.lokalise.sdk.LokaliseContextWrapper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Lokalise.updateTranslations()
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(LokaliseContextWrapper.wrap(newBase ?: return))
     }
 }
